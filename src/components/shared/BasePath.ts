f6108746a4c9224625726c6629ab2047cb080b52
Moin Afzal <mmafzal.fcc@gmail.com>
2023-07-10 14:02:18 +0500

    Resoled path issue for products page

--- src/components/shared/BasePath.ts
+++ src/components/shared/BasePath.ts
@@ -1,3 +1,5 @@
-const BASE_PATH_FORAPI = "http://localhost:3000"
+const BASE_PATH_FORAPI = process.env.NODE_ENV === "development" ?
+    "http://localhost:3000"
+    : "https://commerece-e.vercel.app"
 
-export default BASE_PATH_FORAPI ;
+export default BASE_PATH_FORAPI
